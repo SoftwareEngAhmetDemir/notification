1da7bdaab4359b5ecfe32c79f0923c8515209ac5
Ahmet Demir <43875085+SoftwareEngAhmetDemir@users.noreply.github.com>
2021-07-14 18:23:59 +0300

    Create deneme.js

---
+++ deneme.js
@@ -0,0 +1,11 @@
+Notification.requestPermission();
+
+
+
+// document.getElementById("btn").onclick = ()=>{
+    console.log('clicked')
+    new Notification('Hi, How are you?',{
+        body: 'Have a good day',
+        icon: ''
+      });
+// }
